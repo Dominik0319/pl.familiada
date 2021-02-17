1d2803234ed27066cd882a9a52da73d46c81b20c
Dominik0319 <dominik.skrobisz1@gmail.com>
2021-02-17 21:42:26 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class App {
+public static void main(String[] args) throws Exception {  
+System.out.println( "Witaj w grze familiada" );
+Scanner scanner = new Scanner(System.in);  
+String[][] dane = 
+{
+  {"Więcej niż jedno zwierzę","stado","klucz","dwa","owca","lama"},
+  {"Sporty na s","sztafeta","skok o tyczce", "skoki narciarskie", "sumo", "szachy"}
+};
+  int wynik = 0;
+for(int i = 0; i < dane.length; i++){
+  System.out.println(dane[i][0]); 
+  String odp = scanner.nextLine();
+  boolean isAnswerCorrect = false;
+for(int coś = 1; coś<dane[i].length; coś++){   
+  if(dane[i][coś].equals(odp)){
+  isAnswerCorrect = true;
+  System.out.println( "BRZDĘK" );
+  wynik += coś*10;
+}
+}
+  if(!isAnswerCorrect){
+  System.out.println("X");
+}
+}      
+  System.out.println("Twój wynik to "+wynik);
+  scanner.close();
+}
+}
